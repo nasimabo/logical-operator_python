d5a0279a07f981e0aa628749702460606106010a
nasimabo <nasimuddin555k@gmail.com>
2024-02-23 00:36:15 +0600

    logical operator

---
+++ logical operator.py
@@ -0,0 +1,10 @@
+######### Logical operator ###########
+num = 10
+num2 = 20
+
+#print(not True)
+#print(not False)
+
+print(not (num < num2))
+print(num < num2 or num == num2)
+print(num < num2 and num == num2)
